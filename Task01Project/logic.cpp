9cace9e3847a11e48dd9cff10ba4c7265fc5d9f2
WaitForMeee <sophistcre4yo@gmail.com>
2026-03-02 17:05:32 +0300

    solve task 01

--- Task01Project/logic.cpp
+++ Task01Project/logic.cpp
@@ -8,6 +8,15 @@
 #include "logic.h"
 
 int count_nonzero_elements(int array[DEFAULT_SIZE], int size) {
+	if (size <= 0) {
+		return -1;
+	}
 
-	return 0;
+	int count = 0;
+
+	for (int i = 0; i < size; i++) {
+		array[i] != 0 ? count++ : count;
+	}
+	return count;
+	
 }
