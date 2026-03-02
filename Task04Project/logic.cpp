7989510ab1a3ffb1c250f2383023b2b2eb525a31
WaitForMeee <sophistcre4yo@gmail.com>
2026-03-02 18:48:45 +0300

    Uodate  task05

--- Task04Project/logic.cpp
+++ Task04Project/logic.cpp
@@ -5,3 +5,30 @@
 // и реализовать функцию, которая меняет экстремальные элементы 
 // вектора местами. Если экстремальных элементов несколько, 
 // то необходимо поменять первые найденные элементы.
+
+#include "logic.h"
+
+void swap_extrem_elements(int array[DEFAULT_SIZE], int size) {
+	if (size <= 0) {
+		return ;
+	}
+
+	int max = 0;
+	int min = 0;
+
+	for (int i = 0; i < size; i++) {
+		if (array[i] > array[max]) {
+			max = i;
+		}
+
+		if (array[i] < array[min]) {
+			min = i;
+		}
+
+	}
+
+	int temp = array[min];
+	array[min] = array[max];
+	array[max] = temp;
+
+}
--- Task05Project/logic.cpp
+++ Task05Project/logic.cpp
@@ -4,3 +4,23 @@
 // Дан вектор целочисленных значений. Необходимо спроектировать 
 // и реализовать функцию, которая реверсирует элементы вектора 
 // начиная с позиции a и заканчивая позицией b, включая данные позиции.
+#include "logic.h"
+
+
+void reverse(int array[DEFAULT_SIZE], int size, int a, int b) {
+	if (a > b) {
+		int c = a;
+		a = b;
+		b = c;
+	}
+
+
+	while (a < b) {
+		int c = array[a];
+		array[a] = array[b];
+		array[b] = c;
+		a++;
+		b--;
+	}
+
+}
